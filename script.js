54a8233e2790c757d30f99bfeca77f5bd8dceff4
Kalia Vang <kaliavang511@gmail.com>
2024-05-03 10:42:36 -0500

    got fire and snow btn to work

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-onReady();
+function makeFire(){
+  let fireDiv = document.getElementById("fireDiv")
+  fireDiv.innerHTML += `
+    <span onClick="deletePotato(event)">
+    🔥
+    </span>
+  `
+}
 
-function onReady() {
-  console.log('Javascript is working!');
+function MakeSnowFlake(){
+  let fireDiv = document.getElementById("fireDiv")
+  //grabbing the div we created in html
+  fireDiv.innerHTML += 
+  //adding it in this span
+  `
+    <span onClick="deletePotato(event)">
+    ❄️
+    </span>
+  `
+
 }
